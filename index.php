56ae3783137e2265e6803c5d9697616ff57b053d
Azlator <maximous.grom@gmail.com>
2020-05-27 14:49:40 +0600

    Задача 2

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+$rows = 10;
+$cols = 10;
+echo '<table border="10">';
+for ($tr=1; $tr<=$rows; $tr++){
+    echo '<tr>';
+    for ($td=1; $td<=$cols; $td++){
+        // счётчику $tr.
+        echo '<td width="30" height="30" bgcolor="#ebebeb">'. $tr*$td .'</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+
+?>
